aa70935291c56f8c97d3a3c7c7ba400887b68d4b
Astra <107755845+LilyMusic@users.noreply.github.com>
2022-06-24 18:10:29 +0530

    Update Disconnect.js

--- SlashCommands/MUSIC/Disconnect.js
+++ SlashCommands/MUSIC/Disconnect.js
@@ -2,10 +2,11 @@ const { MessageEmbed } = require('discord.js')
 const { ApplicationCommandOptionType } = require('discord-api-types/v9');
 
 module.exports = {
-  name: "disconnection",
-  description: "stops the player!",
+  name: "disconnect",
+  description: "disconnect the bot!",
   inVc: true,
   sameVc: true,
+player:true
   
   run: async (client, interaction, args) => {
    const player = client.poru.players.get(interaction.guild.id);
@@ -14,4 +15,4 @@ module.exports = {
     return interaction.reply({ embeds: [{ color: 'WHITE', description: '<:checkmark:912259227060437012> |  Disconnected the player!'}]})
     
   }
-} // try it 
+} 
